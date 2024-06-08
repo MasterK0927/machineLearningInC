2167a2146b681e57821d0e42e9901245fca7e0f0
MasterK0927 <btech10189.22@bitmesra.ac.in>
2024-06-08 14:25:05 +0530

    day1 -> training a simple single neuron single connection model.

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#enabling all the warnings using -Wall and -Wrxtra for extra warnings
+
+#enable tracing for seeing whats going on
+set -xe
+clang -Wall -Wextra -o main predictingNumberWithBias.c
