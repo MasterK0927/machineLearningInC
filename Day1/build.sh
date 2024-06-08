9fdae0da58f85d3ffcd51252241444898b29285b
MasterK0927 <btech10189.22@bitmesra.ac.in>
2024-06-08 18:01:35 +0530

    teoInputParameters and single neuron

--- Day1/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#enabling all the warnings using -Wall and -Wrxtra for extra warnings
-
-#enable tracing for seeing whats going on
-set -xe
-clang -Wall -Wextra -o main predictingNumberWithBias.c
